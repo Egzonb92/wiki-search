6911081d11a4c5a4e19f7a6e71d2e402c2e0f254
Egzonb92 <63206951+Egzonb92@users.noreply.github.com>
2022-09-03 12:49:01 +0200

    bug fix

--- src/wiki-search-components/results/api/API.js
+++ src/wiki-search-components/results/api/API.js
@@ -1,5 +1,5 @@
 const fetchWiki = (searchWord) => {
-    var url = `https://en.wikipesdia.org/w/api.php?action=query&origin=*&format=json&generator=search&prop=pageimages|extracts&exintro&explaintext&exchars=200&pithumbsize=100&gsrnamespace=0&gsrlimit=5&gsrsearch='${searchWord}'`;
+    var url = `https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&prop=pageimages|extracts&exintro&explaintext&exchars=200&pithumbsize=100&gsrnamespace=0&gsrlimit=5&gsrsearch='${searchWord}'`;
     const abortCtrl = new AbortController()
     return [window.fetch(url, {signal:abortCtrl.signal}).then(async (response) => {
         const data = await response.json();
