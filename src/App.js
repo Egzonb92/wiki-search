181ab3777be4c30e1cc8dffbeaafc03cf4fb43d2
Egzonb92 <63206951+Egzonb92@users.noreply.github.com>
2022-09-05 20:22:32 +0200

    added bug

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import React from "react";
 import {Header} from "./wiki-search-components/header/Header";
 import {Form} from "./wiki-search-components/form/Form";
 import {Results} from "./wiki-search-components/results/results";
+import flie from "./flie.png"
+
+export const Flie = () => {
+    const x = Math.random()*100
+    const y = Math.random()*100
+    return (
+        <img style={{
+            width:"3rem",
+            height:"2.5rem",
+            objectFit: "cover",
+            objectPosition:"right",
+            background:"none",
+            position:"absolute",
+            filter: "brightness(1.75)",
+            top:`${y}vh`,
+            left:`${x}vw`
+
+        }
+        } src={flie}/>
+    )
+}
+
 
 function App() {
 
@@ -15,19 +37,21 @@ function App() {
         setSearchWord(newSearchWord);
     };
 
-    const clear  = () => {
+    const clear = () => {
         setSearchResults(null);
         setSearchWord("")
-        try{localStorage.removeItem("Wiki-Search-Results")}
-        catch (e){
+        try {
+            localStorage.removeItem("Wiki-Search-Results")
+        } catch (e) {
 
         }
     }
     return (
         <div className="App">
+            <Flie/>
             <Header/>
-                <Form initialSearch={!searchResults} onSubmit={handleSubmit} clear={clear}/>
-                <Results searchWord={searchWord} searchResults={searchResults} setSearchResults={setSearchResults}/>
+            <Form initialSearch={!searchResults} onSubmit={handleSubmit} clear={clear}/>
+            <Results searchWord={searchWord} searchResults={searchResults} setSearchResults={setSearchResults}/>
         </div>
     );
 }
